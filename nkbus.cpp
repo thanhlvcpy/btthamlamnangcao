65de2fa9f9f8646a6b1eb8b1ef4aa61f2037ceeb
thanhlvcpy <42794074+thanhlvcpy@users.noreply.github.com>
2019-03-19 21:14:37 +0700

    Add files via upload

---
+++ nkbus.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//NKBUS,,,Don xe buyt
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int d=0,timewasted[200001],tongthoigian=0;
+    for(int i=1;i<=n;i++)
+    {
+       int t,k,nhanvien;
+       cin>>t>>k;
+       for(int i=1;i<=k;i++)
+       {
+           cin>>nhanvien;
+           d++;
+           timewasted[d]=nhanvien-tongthoigian;
+       }
+       tongthoigian=tongthoigian+t;
+
+    }
+    sort(timewasted+1,timewasted+d+1);
+    cout<<tongthoigian+timewasted[m];
+    return 0;
+}
